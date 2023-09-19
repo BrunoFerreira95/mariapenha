bfa3de4fb03f3a22a757e28c97117bb073adcb26
Joaopedronicolau <joaopedronicolau87@gmail.com>
2023-09-19 16:20:48 -0300

    Style: Form usuários

--- src/app/admin/usuario/page.tsx
+++ src/app/admin/usuario/page.tsx
@@ -1,39 +1,69 @@
-'use client'
-import Topoonda from '@/components/Header'
-import { fetchUserData } from '@/controler/admin/users/users.controler'
-import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
-import Voltar from '@/components/voltar'
-import ButtonVoltar from '@/components/voltar'
+"use client";
+import Topoonda from "@/components/Header";
+import { fetchUserData } from "@/controler/admin/users/users.controler";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Lapis from "@assets/lapis.svg";
+import Voltar from "@/components/voltar";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+import ButtonVoltar from "@/components/voltar";
 
 type UsersProps = {
-    email: string
-    id: string
-
-}[]
+  email: string;
+  id: string;
+}[];
 
 const SemPermissoes = () => {
-    const [users, setUsers] = useState<UsersProps>([])
-    useEffect(() => {
-        fetchUserData(setUsers)
-    }, [])
-    return (
-        <>
-            <div className='flex flex-col min-h-screen max-h-fit'>
-                <Topoonda />
-                <ButtonVoltar/>
-                <div className='flex justify-center items-center flex-col mt-52 gap-5'>
-                    {users.map((usuario) => (
-                        <Link href={`./usuario/formulario?id=${usuario.id}`} key={usuario.id}>
-                            <button className='border-black border-2'>{usuario.email}</button>
-                        </Link>
-                    ))}
+  const [users, setUsers] = useState<UsersProps>([]);
+  useEffect(() => {
+    fetchUserData(setUsers);
+  }, []);
+  return (
+    <>
+      <div className="flex flex-col min-h-screen max-h-fit">
+        <Topoonda />
+        <ButtonVoltar />
+        <div className="flex justify-center">
+          <div className="w-1/4 flex justify-center">
+            <Table>
+              <TableHeader>
+                <TableRow className="">
+                  <TableHead>Usuários</TableHead>
+                  <TableHead>Ações</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {users.map((usuario) => (
+                  <TableRow key={usuario.id}>
+                    <TableCell className="font-medium">
+                        {usuario.email}
+                    </TableCell>
+                    <TableCell className="">
+                      <Link href={`./usuario/formulario?id=${usuario.id}`}>
+                        <button>
+                        <Image className="ml-3" src={Lapis} alt="editar" />
+                        </button>
+                      </Link>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-                </div>
-            </div>
-        </>
-    )
-}
-
-
-export default SemPermissoes
+export default SemPermissoes;
