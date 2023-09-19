49cbddc67b2fb9bcdbb20ba6b783dabf81cb7175
Mary <marymendesblog@gmail.com>
2023-09-19 17:16:28 -0300

    feat:Id

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -6,14 +6,15 @@ type positionprops={
     name: string;
     lat: number;
     lng: number;
+    id:number;
 }[]
 
 const policeLocations: positionprops =  [
-    { name: "GCM", lat: -22.97794717549575, lng: -49.86824625696814 },
-    { name: "SEDE GCM", lat: -22.985907706493986, lng: -49.86897232165561 },
-    { name: "DELEGACIA DA MULHER", lat: -22.97966733718405, lng: -49.87410952060253 },
-    { name: "POLÍCIA MILITAR", lat: -22.965902372024612, lng: -49.865990865022596 },
-    { name: "GCM 2", lat: -22.97890047462074, lng: -49.87858783409708 },
+    { id:1, name: "GCM", lat: -22.97794717549575, lng: -49.86824625696814 },
+    { id:2,name: "SEDE GCM", lat: -22.985907706493986, lng: -49.86897232165561 },
+    { id:3,name: "DELEGACIA DA MULHER", lat: -22.97966733718405, lng: -49.87410952060253 },
+    { id:4,name: "POLÍCIA MILITAR", lat: -22.965902372024612, lng: -49.865990865022596 },
+    { id:5,name: "GCM 2", lat: -22.97890047462074, lng: -49.87858783409708 },
   ];
 
 
@@ -24,7 +25,7 @@ const Map = () => {
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
             {policeLocations.map((location)=>(
 
-            <Marker position={[location.lat, location.lng]}>
+            <Marker key={location.id} position={[location.lat, location.lng]}>
                 <Popup>
                     {location.name}
                 </Popup>
