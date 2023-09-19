b8f94388fe933ea761b4625b204be10b34125630
Mary <marymendesblog@gmail.com>
2023-09-19 14:50:23 -0300

    feat:APIMapa

--- src/app/usuario/rota/page.tsx
+++ src/app/usuario/rota/page.tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import Menumaria from "../../../components/Menumaria";
 import Header from "@/components/Header";
 import Logo from '@/components/logo'; 
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const Contatos = () => {
   const [name, setName] = useState("");
@@ -18,72 +20,30 @@ const Contatos = () => {
   const handleViewPolicePoints = (e) => {
     e.preventDefault();
 
-    // Simulando a busca de pontos de policiamento próximos.
-    // Neste exemplo, usamos um array de objetos.
-    const mockPolicePoints = [
-      
-      {
-        name: "GCM",
-        address: "Praça Mello Peixoto, s/n - Centro, Ourinhos - SP, 19900-030",
-      },
-      {
-        name: "SEDE GCM",
-        address: "R. Silva Jardim, 164 - Vila Emilia, Ourinhos - SP, 19900-191",
-      },
-      {
-        name: "DELEGACIA DA MULHER",
-        address:
-          "R. Aristídes Lau Sampaio, 159 - Jardim Paulista, Ourinhos - SP, 19907-090",
-      },
-      {
-        name: "POLÍCIA MILITAR",
-        address:
-        "Av. Domingos Perino, 1055 - Vila Perino, Ourinhos - SP, 19911-781",
-      },
-      {
-        name: "GCM 2",
-        address: "R. Pedro Marques de Leão, 484-654 - Jardim Paulista, Ourinhos - SP, 19907-010",
-      },
-      
-      
+    const policeLocations = [
+      { name: "GCM", lat: -22.97794717549575, lng: -49.86824625696814 },
+      { name: "SEDE GCM", lat: -22.985907706493986, lng: -49.86897232165561 },
+      { name: "DELEGACIA DA MULHER", lat: -22.97966733718405, lng: -49.87410952060253 },
+      { name: "POLÍCIA MILITAR", lat: -22.965902372024612, lng: -49.865990865022596 },
+      { name: "GCM 2", lat: -22.97890047462074, lng: -49.87858783409708 },
     ];
 
-    setPolicePoints(mockPolicePoints);
+    setPolicePoints(policeLocations);
     setShowPolicePoints(true);
-
-    // Abrir o Google Maps com os pontos de policiamento próximos
-    const googleMapsLink = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
-      mockPolicePoints.map((point) => point.address).join("|")
-    )}`;
-    window.open(googleMapsLink, "_blank");
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // Aqui você pode adicionar a lógica para enviar a denúncia para o servidor.
     console.log("Nome:", name);
     console.log("Endereço:", address);
     console.log("Telefone:", phone);
     console.log("Mensagem:", message);
 
-    // Limpar os campos após o envio (opcional)
     setName("");
     setAddress("");
     setPhone("");
     setMessage("");
-
-    // Exemplo de notificação após o envio (você pode personalizar ou remover)
-    toast.success("Denúncia enviada com sucesso!", {
-      position: "top-center",
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
-    });
   };
 
   return (
@@ -92,50 +52,55 @@ const Contatos = () => {
         <Header />
 
         <div className="mt-4">
-         
-            <div className="flex justify-center">
-              {/* Conteúdo do formulário */}
-            </div>
-            <div>
-              <table className="table-auto flex justify-center">
-                <thead>
-                  {/* Cabeçalho da tabela */}
-                  
-                </thead>
-                <tbody>
-                  {/* Linhas da tabela com cores neutras */}
-                  <tr className="bg-gray-100">
-                    <td className="px-2 py-2 whitespace-nowrap">PM</td>
-                    <td className="px-2 py-2 whitespace-nowrap">190</td>
-                  </tr>
-                  <tr className="bg-gray-200">
-                    <td className="px-2 py-2 whitespace-nowrap">GCM 1</td>
-                    <td className="px-2 py-2 whitespace-nowrap">153</td>
-                  </tr>
-                  <tr className="bg-gray-100">
-                    <td className="px-2 py-2 whitespace-nowrap">GCM 2</td>
-                    <td className="px-2 py-2 whitespace-nowrap">(14) 3335-9320</td>
-                  </tr>
-                  <tr className="bg-gray-200">
-                    <td className="px-2 py-2 whitespace-nowrap">Delegacia da Mulher</td>
-                    <td className="px-2 py-2 whitespace-nowrap">(14) 3322-5343</td>
-                  </tr>
-                  {/* Adicione mais linhas da tabela conforme necessário */}
-                </tbody>
-              </table>
-            </div>
+          <div>
+            <table className="table-auto flex justify-center">
+              <thead></thead>
+              <tbody>
+                <tr className="bg-gray-100">
+                  <td className="px-2 py-2 whitespace-nowrap">PM</td>
+                  <td className="px-2 py-2 whitespace-nowrap">190</td>
+                </tr>
+                <tr className="bg-gray-200">
+                  <td className="px-2 py-2 whitespace-nowrap">GCM 1</td>
+                  <td className="px-2 py-2 whitespace-nowrap">153</td>
+                </tr>
+                <tr className="bg-gray-100">
+                  <td className="px-2 py-2 whitespace-nowrap">GCM 2</td>
+                  <td className="px-2 py-2 whitespace-nowrap">(14) 3335-9320</td>
+                </tr>
+                <tr className="bg-gray-200">
+                  <td className="px-2 py-2 whitespace-nowrap">Delegacia da Mulher</td>
+                  <td className="px-2 py-2 whitespace-nowrap">(14) 3322-5343</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
 
-            {/* Adicione o botão "Visualizar Pontos de Policiamento" e configure o link */}
-            <div className="flex justify-center mt-4">
-              <button
-                type="button"
-                onClick={handleViewPolicePoints}
-                className="px-2 py-2 bg-purple-500 text-white rounded-md mt-4"
-              >
-                Visualizar Pontos de Policiamento
-              </button>
+          <div className="flex justify-center mt-4">
+            <button
+              type="button"
+              onClick={handleViewPolicePoints}
+              className="px-2 py-2 bg-purple-500 text-white rounded-md mt-4"
+            >
+              Visualizar Pontos de Policiamento
+            </button>
+          </div>
+
+          {showPolicePoints && (
+            <div className="mt-4">
+              <MapContainer center={[-22.977983, -49.873451]} zoom={13} style={{ height: "400px" }}>
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {policePoints.map((point, index) => (
+                  <Marker key={index} position={[point.lat, point.lng]}>
+                    <Popup>{point.name}</Popup>
+                  </Marker>
+                ))}
+              </MapContainer>
             </div>
-          
+          )}
         </div>
       </div>
 
