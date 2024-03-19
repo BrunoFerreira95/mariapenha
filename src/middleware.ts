b40b040e0961206639da1851fc262fc3aebef4e3
Bruno Ferreira Pedraça <47322135+BrunoFerreira95@users.noreply.github.com>
2024-03-19 18:46:49 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,27 +8,27 @@ export async function middleware(req: NextRequest) {
   const session = await supabase.auth.getSession();
 
 
-  // if (req.nextUrl.pathname.startsWith("/admin")) {
-  //   // Verifica se a sessão existe e se o campo app_metadata.claims_admin está definido
-  //   if (!session || !session.data.session || !session.data.session.user || !session.data.session.user.app_metadata || !session.data.session.user.app_metadata.claims_admin) {
-  //     return NextResponse.redirect(`${requestUrl.origin}/404`);
-  //   }
-  // }
+  if (req.nextUrl.pathname.startsWith("/admin")) {
+    // Verifica se a sessão existe e se o campo app_metadata.claims_admin está definido
+    if (!session || !session.data.session || !session.data.session.user || !session.data.session.user.app_metadata || !session.data.session.user.app_metadata.claims_admin) {
+      return NextResponse.redirect(`${requestUrl.origin}/404`);
+    }
+  }
   
 
-  // if (req.nextUrl.pathname.startsWith("/guarda")) {
-  //   // Verifica se a sessão existe e se o campo app_metadata.usuario está definido
-  //   if (!session || !session.data.session || !session.data.session.user || !session.data.session.user.app_metadata || !session.data.session.user.app_metadata.usuario || session.data.session.user.app_metadata.usuario == 'guarda') {
-  //     return NextResponse.redirect(`${requestUrl.origin}/404`);
-  //   }
-  // }
+  if (req.nextUrl.pathname.startsWith("/guarda")) {
+    // Verifica se a sessão existe e se o campo app_metadata.usuario está definido
+    if (!session || !session.data.session || !session.data.session.user || !session.data.session.user.app_metadata || !session.data.session.user.app_metadata.usuario || session.data.session.user.app_metadata.usuario == 'guarda') {
+      return NextResponse.redirect(`${requestUrl.origin}/404`);
+    }
+  }
 
-  // if (req.nextUrl.pathname.startsWith("/usuario")) {
-  //   // Verifica se a sessão existe e se o campo app_metadata.usuario está definido
-  //   if (!session || !session.data.session || !session.data.session.user || !session.data.session.user.app_metadata || !session.data.session.user.app_metadata.usuario || session.data.session.user.app_metadata.usuario == 'maria') {
-  //     return NextResponse.redirect(`${requestUrl.origin}/404`);
-  //   }
-  // }
+  if (req.nextUrl.pathname.startsWith("/usuario")) {
+    // Verifica se a sessão existe e se o campo app_metadata.usuario está definido
+    if (!session || !session.data.session || !session.data.session.user || !session.data.session.user.app_metadata || !session.data.session.user.app_metadata.usuario || session.data.session.user.app_metadata.usuario == 'maria') {
+      return NextResponse.redirect(`${requestUrl.origin}/404`);
+    }
+  }
   
   return res;
 }
